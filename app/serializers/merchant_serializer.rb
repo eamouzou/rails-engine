8061a2e50e922b502fb15471560e0418e2d36b34
Elom Amouzou <elom1990@yahoo.com>
2020-03-17 22:00:39 -0600

    add merchant serializer with relationships

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,4 +1,9 @@
 class MerchantSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name
+
+  has_many :items
+  has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 end
