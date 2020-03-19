4ca11f97629d9d82b572e0e421c1c5c37bdba3a3
Elom Amouzou <elom1990@yahoo.com>
2020-03-19 06:53:44 -0600

    add item controller with actions; all tests pass

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,32 @@
+# app/controllers/api/v1/items_controller.rb
+
+class Api::V1::ItemsController < ApplicationController
+  def index
+    render json: ItemSerializer.new(Item.all)
+  end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    merchant = Merchant.find(params[:item][:merchant_id])
+    render json: ItemSerializer.new(merchant.items.create(item_params))
+  end
+
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  def destroy
+    render json: ItemSerializer.new(Item.find(params[:id]))
+    Item.delete(params[:id])
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+end
