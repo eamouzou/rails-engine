f997ce0631fa10fcfd5a8322ec5a893b20cc672f
Elom Amouzou <elom1990@yahoo.com>
2020-03-21 01:48:55 -0600

    add routes for items find and find all

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
+      namespace :items do
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+      end
+
       resources :merchants, except: [:new, :edit] do
         resources :items, only: [:index]
       end
