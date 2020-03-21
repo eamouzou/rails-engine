0fda54b84bb887563a0ca3c3b85006526168eb4b
Elom Amouzou <elom1990@yahoo.com>
2020-03-21 01:50:18 -0600

    create controller and actions (index, show) for items search; tests pass

---
+++ app/controllers/api/v1/items/search_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::Items::SearchController < ApplicationController
+
+  def index
+    render json: ItemSerializer.new(Item.where(request.query_parameters))
+  end
+
+  def show
+    render json: ItemSerializer.new(Item.find_by(request.query_parameters))
+  end
+
+end
