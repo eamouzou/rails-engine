64b92082c82e8d610fc2432218559896b1c113ee
Elom Amouzou <elom1990@yahoo.com>
2020-03-22 18:28:28 -0600

    add most revenue class method; all tests pass

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,15 @@ class Merchant < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.most_revenue(quantity)
+    quantity = quantity.to_i
+    joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: "success"})
+    .select('merchants.*, sum(invoice_items.unit_price*invoice_items.quantity) as revenue')
+    .group(:id)
+    .order(revenue: :desc)
+    .limit(quantity)
+  end
+
 end
